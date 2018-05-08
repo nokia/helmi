c52271c6adb846884192427240daa8b55d481e9d
Sebastian Wicki <sebastian@monostream.com>
2018-05-08 19:24:11 +0200

    Implement new template engine
    Switches the template engine for for the catalog to text/template
    with sprig.

--- pkg/catalog/catalog.go
+++ pkg/catalog/catalog.go
@@ -1,17 +1,43 @@
 package catalog
 
 import (
-	"log"
-	"strings"
-	"io/ioutil"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"fmt"
+	"os"
+
+	"bytes"
+	"github.com/Masterminds/sprig"
+	"github.com/monostream/helmi/pkg/helm"
+	"github.com/monostream/helmi/pkg/kubectl"
+	"github.com/satori/go.uuid"
+	"strconv"
 )
 
 type Catalog struct {
-	Services []CatalogService `yaml:"services"`
+	Services map[string]Service
 }
 
-type CatalogService struct {
+type Service struct {
+	Id          string `yaml:"_id"`
+	Name        string `yaml:"_name"`
+	Description string `yaml:"description"`
+
+	Chart        string `yaml:"chart"`
+	ChartVersion string `yaml:"chart-version"`
+
+	Plans []Plan `yaml:"plans"`
+
+	valuesTemplate      *template.Template
+	credentialsTemplate *template.Template
+}
+
+type Plan struct {
 	Id          string `yaml:"_id"`
 	Name        string `yaml:"_name"`
 	Description string `yaml:"description"`
@@ -21,60 +47,232 @@ type CatalogService struct {
 	ChartValues  map[string]string `yaml:"chart-values"`
 
 	UserCredentials map[string]interface{} `yaml:"user-credentials"`
-
-	Plans []CatalogPlan `yaml:"plans"`
 }
 
-type CatalogPlan struct {
-	Id          string `yaml:"_id"`
-	Name        string `yaml:"_name"`
-	Description string `yaml:"description"`
-
-	Chart        string            `yaml:"chart"`
-	ChartVersion string            `yaml:"chart-version"`
-	ChartValues  map[string]string `yaml:"chart-values"`
-
-	UserCredentials map[string]interface{} `yaml:"user-credentials"`
-}
-
-func (c *Catalog) Parse(path string) {
-	input, err := ioutil.ReadFile(path)
-
-	if err != nil {
-		log.Printf("Catalog.Read: #%v ", err)
+// Parses all `.yaml` and `.yml` files in the specified path as service definitions
+func ParseDir(dir string) (Catalog, error) {
+	c := Catalog{
+		Services: make(map[string]Service),
 	}
 
-	// insert fake root to allow parsing
-	data := "services:\n" + string(input)
-	input = []byte(data)
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("unable to read catalog file: %q: %s", path, err)
+			return nil
+		}
 
-	err = yaml.Unmarshal(input, c)
+		ext := filepath.Ext(path)
+		if info.IsDir() || (ext != ".yml" && ext != ".yaml") {
+			return nil
+		}
+
+		input, ioErr := ioutil.ReadFile(path)
+		if ioErr != nil {
+			return ioErr
+		}
+
+		return c.parseServiceDefinition(input, path)
+	})
 
 	if err != nil {
-		log.Fatalf("Catalog.Unmarshal: %v", err)
+		return c, err
 	}
+
+	if len(c.Services) == 0 {
+		err = fmt.Errorf("no services found in catalog directory: %s", dir)
+		return c, err
+	}
+
+	return c, nil
 }
 
-func (c *Catalog) GetService(service string) (CatalogService, error) {
-	for _, s := range c.Services {
-		if strings.EqualFold(s.Id, service) {
-			return s, nil
+func (c *Catalog) parseServiceDefinition(input []byte, file string) error {
+	// we have three documents: service, chart-values, user-credentials
+	documents := bytes.Split(input, []byte("\n---"))
+	if n := len(documents); n != 3 {
+		return fmt.Errorf("service file %s: must contain 3 yaml document parts, found %d", file, n)
+	}
+
+	var s struct{ Service }
+	err := yaml.Unmarshal(documents[0], &s)
+	if err != nil {
+		return fmt.Errorf("failed to parse service definition: %s: %s", file, err)
+	}
+
+	fMap := templateFuncMap()
+	valuesTemplate, valuesErr := template.New("values").Funcs(fMap).Parse(string(documents[1]))
+	if valuesErr != nil {
+		return fmt.Errorf("failed to parse values template: %s: %s", file, valuesErr)
+	}
+
+	credentialsTemplate, credentialsErr := template.New("credentials").Funcs(fMap).Parse(string(documents[2]))
+	if credentialsErr != nil {
+		return fmt.Errorf("failed to parse credentials template: %s: %s", file, credentialsErr)
+	}
+
+	s.valuesTemplate = valuesTemplate
+	s.credentialsTemplate = credentialsTemplate
+
+	c.Services[s.Id] = s.Service
+	return nil
+}
+
+func (c *Catalog) Service(id string) *Service {
+	if val, ok := c.Services[id]; ok {
+		return &val
+	}
+	return nil
+}
+
+func (s *Service) Plan(id string) *Plan {
+	for _, p := range s.Plans {
+		if strings.EqualFold(p.Id, id) {
+			return &p
+		}
+	}
+	return nil
+}
+
+func templateFuncMap() template.FuncMap {
+	f := sprig.TxtFuncMap()
+
+	randomUuid := func() string {
+		s := uuid.NewV4().String()
+		s = strings.Replace(s, "-", "", -1)
+		return s
+	}
+
+	f["generateUsername"] = randomUuid
+	f["generatePassword"] = randomUuid
+
+	return f
+}
+
+type chartValueVars struct {
+	*Service
+	*Plan
+}
+
+func (s *Service) ChartValues(p *Plan) (map[string]string, error) {
+	b := new(bytes.Buffer)
+	data := chartValueVars{s, p}
+	err := s.valuesTemplate.Execute(b, data)
+	if err != nil {
+		return nil, err
+	}
+
+	var v struct {
+		ChartValues map[string]string `yaml:"chart-values"`
+	}
+	err = yaml.Unmarshal(b.Bytes(), &v)
+	if err != nil {
+		return nil, err
+	}
+
+	return v.ChartValues, nil
+}
+
+type credentialVars struct {
+	Service *Service
+	Plan    *Plan
+	Values  valueVars
+	Release releaseVars
+	Cluster clusterVars
+}
+
+type valueVars map[string]string
+
+type releaseVars struct {
+	Name      string
+	Namespace string
+}
+
+type clusterVars struct {
+	Address    string
+	Hostname   string
+	helmStatus helm.Status
+}
+
+func (c clusterVars) Port(port ...int) string {
+	for clusterPort, nodePort := range c.helmStatus.NodePorts {
+		if len(port) == 0 || port[0] == clusterPort {
+			return strconv.Itoa(nodePort)
 		}
 	}
 
-	return *new(CatalogService), nil
-}
-
-func (c *Catalog) GetServicePlan(service string, plan string) (CatalogPlan, error) {
-	for _, s := range c.Services {
-		if strings.EqualFold(s.Id, service) {
-			for _, p := range s.Plans {
-				if strings.EqualFold(p.Id, plan) {
-					return p, nil
-				}
-			}
+	for clusterPort, nodePort := range c.helmStatus.ClusterPorts {
+		if len(port) == 0 || port[0] == clusterPort {
+			return strconv.Itoa(nodePort)
 		}
 	}
 
-	return *new(CatalogPlan), nil
+	if len(port) > 0 {
+		return strconv.Itoa(port[0])
+	}
+
+	return ""
+}
+
+func extractAddress(kubernetesNodes []kubectl.Node) string {
+	// return dns name if set as environment variable
+	if value, ok := os.LookupEnv("DOMAIN"); ok {
+		return value
+	}
+
+	for _, node := range kubernetesNodes {
+		if len(node.ExternalIP) > 0 {
+			return node.ExternalIP
+		}
+	}
+
+	for _, node := range kubernetesNodes {
+		if len(node.InternalIP) > 0 {
+			return node.InternalIP
+		}
+	}
+
+	return ""
+}
+
+func extractHostname(kubernetesNodes []kubectl.Node) string {
+	for _, node := range kubernetesNodes {
+		if len(node.Hostname) > 0 {
+			return node.Hostname
+		}
+	}
+
+	return ""
+}
+
+func (s *Service) UserCredentials(plan *Plan, kubernetesNodes []kubectl.Node, helmStatus helm.Status, values map[string]string) (map[string]interface{}, error) {
+	env := credentialVars{
+		Service: s,
+		Plan:    plan,
+		Values:  values,
+		Release: releaseVars{
+			Name:      helmStatus.Name,
+			Namespace: helmStatus.Namespace,
+		},
+		Cluster: clusterVars{
+			Address:    extractAddress(kubernetesNodes),
+			Hostname:   extractHostname(kubernetesNodes),
+			helmStatus: helmStatus,
+		},
+	}
+
+	b := new(bytes.Buffer)
+	err := s.credentialsTemplate.Execute(b, env)
+	if err != nil {
+		return nil, err
+	}
+
+	var v struct {
+		UserCredentials map[string]interface{} `yaml:"user-credentials"`
+	}
+	err = yaml.Unmarshal(b.Bytes(), &v)
+	if err != nil {
+		return nil, err
+	}
+
+	return v.UserCredentials, nil
 }
--- pkg/release/release.go
+++ pkg/release/release.go
@@ -2,29 +2,13 @@ package release
 
 import (
 	"errors"
-	"regexp"
-	"strings"
-	"strconv"
-	"go.uber.org/zap"
-	"github.com/satori/go.uuid"
+	"github.com/monostream/helmi/pkg/catalog"
 	"github.com/monostream/helmi/pkg/helm"
 	"github.com/monostream/helmi/pkg/kubectl"
-	"github.com/monostream/helmi/pkg/catalog"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"os"
-	"reflect"
+	"strings"
 )
-
-const lookupRegex = `\{\{\s*lookup\s*\(\s*'(?P<type>[\w]+)'\s*,\s*'(?P<path>[\w/:.]+)'\s*\)\s*\}\}`
-const lookupRegexType = "type"
-const lookupRegexPath = "path"
-
-const lookupValue    = "value"
-const lookupCluster  = "cluster"
-const lookupUsername = "username"
-const lookupPassword = "password"
-const lookupEnv      = "env"
-const lookupRelease  = "release"
 
 type Status struct {
 	IsFailed    bool
@@ -47,12 +31,12 @@ func Install(catalog *catalog.Catalog, serviceId string, planId string, id strin
 	name := getName(id)
 	logger := getLogger()
 
-	service, _ := catalog.GetService(serviceId)
-	plan, _ := catalog.GetServicePlan(serviceId, planId)
+	service := catalog.Service(serviceId)
+	plan := service.Plan(planId)
 
 	chart, chartErr := getChart(service, plan)
 	chartVersion, chartVersionErr := getChartVersion(service, plan)
-	chartValues := getChartValues(service, plan)
+	chartValues, valuesErr := service.ChartValues(plan)
 
 	if chartErr != nil {
 		logger.Error("failed to install release",
@@ -67,6 +51,18 @@ func Install(catalog *catalog.Catalog, serviceId string, planId string, id strin
 
 	if chartVersionErr != nil {
 		chartVersion = ""
+	}
+
+
+	if valuesErr != nil {
+		logger.Error("failed to parse chart-values section",
+			zap.String("id", id),
+			zap.String("name", name),
+			zap.String("serviceId", serviceId),
+			zap.String("planId", planId),
+			zap.Error(valuesErr))
+
+		return valuesErr
 	}
 
 	err := helm.Install(name, chart, chartVersion, chartValues, acceptsIncomplete)
@@ -183,8 +179,8 @@ func GetCredentials(catalog *catalog.Catalog, serviceId string, planId string, i
 	name := getName(id)
 	logger := getLogger()
 
-	service, _ := catalog.GetService(serviceId)
-	plan, _ := catalog.GetServicePlan(serviceId, planId)
+	service := catalog.Service(serviceId)
+	plan := service.Plan(planId)
 
 	status, err := helm.GetStatus(name)
 
@@ -229,7 +225,15 @@ func GetCredentials(catalog *catalog.Catalog, serviceId string, planId string, i
 		return nil, err
 	}
 
-	credentials := getUserCredentials(service, plan, nodes, status, values)
+	credentials, err := service.UserCredentials(plan, nodes, status, values)
+	if err != nil {
+		logger.Error("failed to parse user credentials",
+			zap.String("id", id),
+			zap.String("name", name),
+			zap.Error(err))
+
+		return nil, err
+	}
 
 	logger.Debug("sending release credentials",
 		zap.String("id", id),
@@ -252,7 +256,7 @@ func getName(value string) string {
 	return prefix + name[:14]
 }
 
-func getChart(service catalog.CatalogService, plan catalog.CatalogPlan) (string, error) {
+func getChart(service *catalog.Service, plan *catalog.Plan) (string, error) {
 	if len(plan.Chart) > 0 {
 		return plan.Chart, nil
 	}
@@ -264,7 +268,7 @@ func getChart(service catalog.CatalogService, plan catalog.CatalogPlan) (string,
 	return "", errors.New("no helm chart specified")
 }
 
-func getChartVersion(service catalog.CatalogService, plan catalog.CatalogPlan) (string, error) {
+func getChartVersion(service *catalog.Service, plan *catalog.Plan) (string, error) {
 	if len(plan.ChartVersion) > 0 {
 		return plan.ChartVersion, nil
 	}
@@ -274,233 +278,4 @@ func getChartVersion(service catalog.CatalogService, plan catalog.CatalogPlan) (
 	}
 
 	return "", errors.New("no helm chart version specified")
-}
-
-func getChartValues(service catalog.CatalogService, plan catalog.CatalogPlan) map[string]string {
-	values := map[string]string{}
-	templates := map[string]string{}
-
-	for key, value := range service.ChartValues {
-		templates[key] = value
-	}
-
-	for key, value := range plan.ChartValues {
-		templates[key] = value
-	}
-
-	usernames := map[string]string{}
-	passwords := map[string]string{}
-
-	r := regexp.MustCompile(lookupRegex)
-	groupNames := r.SubexpNames()
-
-	for key, template := range templates {
-		value := r.ReplaceAllStringFunc(template, func(m string) string {
-			var lookupType string
-			var lookupPath string
-
-			for groupKey, groupValue := range r.FindStringSubmatch(m) {
-				groupName := groupNames[groupKey]
-
-				if strings.EqualFold(groupName, lookupRegexType) {
-					lookupType = groupValue
-				}
-
-				if strings.EqualFold(groupName, lookupRegexPath) {
-					lookupPath = groupValue
-				}
-			}
-
-			if strings.EqualFold(lookupType, lookupUsername) {
-				username := usernames[lookupPath]
-
-				if len(username) == 0 {
-					username = uuid.NewV4().String()
-					username = strings.Replace(username, "-", "", -1)
-					usernames[lookupPath] = username
-				}
-
-				return username
-			}
-
-			if strings.EqualFold(lookupType, lookupPassword) {
-				password := passwords[lookupPath]
-
-				if len(password) == 0 {
-					password = uuid.NewV4().String()
-					password = strings.Replace(password, "-", "", -1)
-					passwords[lookupPath] = password
-				}
-
-				return password
-			}
-
-			if strings.EqualFold(lookupType, lookupEnv) {
-				env, _ := os.LookupEnv(lookupPath)
-				return env
-			}
-
-			return ""
-		})
-
-		if len(value) > 0 {
-			values[key] = value
-		}
-	}
-
-	return values
-}
-
-func getUserCredentials(service catalog.CatalogService, plan catalog.CatalogPlan, kubernetesNodes [] kubectl.Node, helmStatus helm.Status, helmValues map[string]string) map[string]interface{} {
-	values := map[string]interface{}{}
-	templates := map[string]interface{}{}
-
-	for key, value := range service.UserCredentials {
-		templates[key] = value
-	}
-
-	for key, value := range plan.UserCredentials {
-		templates[key] = value
-	}
-
-	r := regexp.MustCompile(lookupRegex)
-	groupNames := r.SubexpNames()
-
-	replaceTemplate := func(template string) string {
-		var lookupType string
-		var lookupPath string
-
-		for groupKey, groupValue := range r.FindStringSubmatch(template) {
-			groupName := groupNames[groupKey]
-
-			if strings.EqualFold(groupName, lookupRegexType) {
-				lookupType = groupValue
-			}
-
-			if strings.EqualFold(groupName, lookupRegexPath) {
-				lookupPath = groupValue
-			}
-		}
-
-		if strings.EqualFold(lookupType, lookupRelease) {
-			if strings.EqualFold(lookupPath, "name") {
-				return helmStatus.Name
-			}
-			if strings.EqualFold(lookupPath, "namespace") {
-				return helmStatus.Namespace
-			}
-		}
-
-		if strings.EqualFold(lookupType, lookupUsername) {
-			username := helmValues[lookupPath]
-			return username
-		}
-
-		if strings.EqualFold(lookupType, lookupPassword) {
-			password := helmValues[lookupPath]
-			return password
-		}
-
-		if strings.EqualFold(lookupType, lookupValue) {
-			value := helmValues[lookupPath]
-			return value
-		}
-
-		if strings.EqualFold(lookupType, lookupCluster) {
-			if strings.HasPrefix(strings.ToLower(lookupPath), "port") {
-				portParts := strings.Split(lookupPath, ":")
-
-				for clusterPort, nodePort := range helmStatus.NodePorts {
-					if len(portParts) == 1 || strings.EqualFold(strconv.Itoa(clusterPort), portParts[1]) {
-						return strconv.Itoa(nodePort)
-					}
-				}
-
-				for clusterPort, nodePort := range helmStatus.ClusterPorts {
-					if len(portParts) == 1 || strings.EqualFold(strconv.Itoa(clusterPort), portParts[1]) {
-						return strconv.Itoa(nodePort)
-					}
-				}
-
-				if len(portParts) > 0 {
-					return portParts[0]
-				}
-
-				return "0"
-			}
-
-			// single host
-
-			if strings.EqualFold(lookupPath, "address") {
-				// return dns name if set as environment variable
-				if value, ok := os.LookupEnv("DOMAIN"); ok {
-					return value
-				}
-
-				for _, node := range kubernetesNodes {
-					if len(node.ExternalIP) > 0 {
-						return node.ExternalIP
-					}
-				}
-
-				for _, node := range kubernetesNodes {
-					if len(node.InternalIP) > 0 {
-						return node.InternalIP
-					}
-				}
-			}
-
-			if strings.EqualFold(lookupPath, "hostname") {
-				for _, node := range kubernetesNodes {
-					if len(node.Hostname) > 0 {
-						return node.Hostname
-					}
-				}
-			}
-		}
-
-		return ""
-	}
-
-	for key, templateInterface := range templates {
-		// string
-		templateString, ok := reflect.ValueOf(templateInterface).Interface().(string)
-
-		if ok {
-			value := r.ReplaceAllStringFunc(templateString, replaceTemplate)
-
-			if len(value) > 0 {
-				values[key] = value
-			}
-
-			continue
-		}
-
-		// string array
-		templateStringArray, ok := reflect.ValueOf(templateInterface).Interface().([]interface{})
-
-		if ok {
-			valueArray := []string{}
-
-			for _, templateValue := range templateStringArray {
-				templateString, ok := reflect.ValueOf(templateValue).Interface().(string)
-
-				if ok {
-					value := r.ReplaceAllStringFunc(templateString, replaceTemplate)
-
-					if len(value) > 0 {
-						valueArray = append(valueArray, value)
-					}
-				}
-			}
-
-			if len(valueArray) > 0 {
-				values[key] = valueArray
-			}
-
-			continue
-		}
-	}
-
-	return values
 }
